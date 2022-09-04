38a689c9dc46910c708645cc49ca4727d200fa52
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-09-04 00:26:21 -0500

    eventos part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  // title = 'my-store';
+  name: string = 'Kevin';
+  age: number = 29;
+  img: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled: boolean = true;
+
+  person = {
+    name: 'Kevin',
+    age: 29,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
 }
